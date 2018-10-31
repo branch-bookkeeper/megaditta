608b2cd73c387f9e4e81b794f98acf83a972ff39
Gabriele Genta <gabriele.genta@gmail.com>
2018-11-01 00:00:08 +0100

    Adjust pricing

--- src/components/sections/Pricing.js
+++ src/components/sections/Pricing.js
@@ -1,100 +1,177 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => (
-    <section id="pricing">
-        <div className="row section-head">
-            <h1>Pricing Tables.</h1>
-            <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-                sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-            </p>
-        </div>
-        <div className="row">
-            <div className="pricing-tables bgrid-quarters s-bgrid-halves">
-                <div className="column">
-                    <div className="price-block">
-                        <h3 className="plan-title"><i className="fa fa-cog" />Starter</h3>
-                        <p className="plan-price">FREE <span>per month</span></p>
+import { bbOrange, text } from 'style/colors';
 
-                        <ul className="features">
-                            <li>1GB Storage</li>
-                            <li>5GB Bandwidth</li>
-                            <li>2 Domains</li>
-                            <li>3 Databases</li>
-                            <li>1 FTP Account</li>
-                            <li>25 Email Accounts</li>
-                        </ul>
+const Root = styled.section`
+    &#pricing {
+        h1 {
+            margin-bottom: 54px;
+        }
 
-                        <footer className="plan-sign-up">
-                            <a className="button" href="">Sign Up</a>
-                        </footer>
-                    </div>
+        p {
+            color: ${text};
+            margin-bottom: 0;
+        }
+
+        .button {
+            background-color: ${bbOrange[500]};
+            &:hover {
+                background-color: ${bbOrange[800]};
+            }
+        }
+
+        .pricing-tables {
+            /* Fix layout gimmick */
+            @media only screen and (max-width: 900px) {
+                width: auto;
+            }
+        }
+
+        .plan-price.crossed {
+            span.price {
+                font-family: inherit;
+                font-size: inherit;
+                color: rgba(51, 51, 51, 0.6);
+                display: inline-block;
+                position: relative;
+
+                span.month {
+                    display: inline;
+                }
+
+                &::before, &::after {
+                    content: ' ';
+                    display: block;
+                    height: 6px;
+                    position: absolute;
+                    left: 0;
+                    right: 0;
+                    top: 50%;
+                    margin-top: -3px;
+                    background: #d11d1d;
+                    border-radius: 3px;
+                }
+
+                &::before {
+                    transform: scale(1.2) rotate(15deg);
+                }
+
+                &::after {
+                    transform: scale(1.2) rotate(-15deg);
+                }
+            }
+
+            span.more-info {
+                color: inherit;
+            }
+        }
+
+        .row.open-source {
+            color: ${text};
+            a {
+                transition: none;
+                color: ${text};
+                text-decoration: underline;
+                text-decoration-style: dotted;
+                text-decoration-color: #bbb;
+                text-underline-position: under;
+                &:hover,
+                &:active {
+                    text-decoration-style: solid;
+                    text-decoration-color: ${bbOrange[500]};
+                    color: ${bbOrange[500]};
+                }
+            }
+        }
+    }
+`;
+
+const renderPlans = () => (
+    <div className="row">
+        <div className="pricing-tables bgrid-thirds">
+            <div className="column">
+                <div className="price-block">
+                    <h3 className="plan-title">Startup</h3>
+                    <p className="plan-price crossed">
+                        <span className="price">$9.99 <span className="month">/mo</span></span>
+                        <span className="more-info">FREE while in beta</span>
+                    </p>
+
+                    <ul className="features">
+                        <li>3 private repos</li>
+                        <li>3 users</li>
+                        <li>Max 3 PRs per queue</li>
+                        <li>Queue only in main branch</li>
+                    </ul>
+
+                    <footer className="plan-sign-up">
+                        <a className="button" href="https://get.branch-bookkeeper.com">Install Now</a>
+                    </footer>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="price-block">
+                    <h3 className="plan-title">Premium</h3>
+                    <p className="plan-price crossed">
+                        <span className="price">$19.99 <span className="month">/mo</span></span>
+                        <span className="more-info">FREE while in beta</span>
+                    </p>
+
+                    <ul className="features">
+                        <li>10 private repos</li>
+                        <li>10 users</li>
+                        <li>Ulimited queue length</li>
+                        <li>Queue in any branch</li>
+                    </ul>
+
+                    <footer className="plan-sign-up">
+                        <a className="button" href="https://get.branch-bookkeeper.com">Install Now</a>
+                    </footer>
                 </div>
 
-                <div className="column">
-                    <div className="price-block">
-                        <h3 className="plan-title"><i className="fa fa-thumbs-up" />Standard</h3>
-                        <p className="plan-price">$9.99 <span>per month</span></p>
+            </div>
 
-                        <ul className="features">
-                            <li>3GB Storage</li>
-                            <li>10GB Bandwidth</li>
-                            <li>3 Domains</li>
-                            <li>5 Databases</li>
-                            <li>3 FTP Account</li>
-                            <li>30 Email Accounts</li>
-                        </ul>
+            <div className="column">
+                <div className="price-block">
+                    <h3 className="plan-title">Ultimate</h3>
+                    <p className="plan-price crossed">
+                        <span className="price">$29.99 <span className="month">/mo</span></span>
+                        <span className="more-info">FREE while in beta</span>
+                    </p>
 
-                        <footer className="plan-sign-up">
-                            <a className="button" href="">Sign Up</a>
-                        </footer>
-                    </div>
-                </div>
+                    <ul className="features">
+                        <li>Unlimited private repos</li>
+                        <li>50 users</li>
+                        <li>Ulimited queue length</li>
+                        <li>Queue in any branch</li>
+                    </ul>
 
-                <div className="column">
-                    <div className="price-block">
-                        <h3 className="plan-title"><i className="fa fa-star" />Premium</h3>
-                        <p className="plan-price">$19.99 <span>per month</span></p>
-
-                        <ul className="features">
-                            <li>10GB Storage</li>
-                            <li>25GB Bandwidth</li>
-                            <li>5 Domains</li>
-                            <li>10 Databases</li>
-                            <li>10 FTP Account</li>
-                            <li>50 Email Accounts</li>
-                        </ul>
-
-                        <footer className="plan-sign-up">
-                            <a className="button" href="">Sign Up</a>
-                        </footer>
-                    </div>
-
-                </div>
-
-                <div className="column">
-                    <div className="price-block">
-                        <h3 className="plan-title"><i className="fa fa-trophy" />Ultimate</h3>
-                        <p className="plan-price">$29.99 <span>per month</span></p>
-
-                        <ul className="features">
-                            <li>30 Storage</li>
-                            <li>Unlimited Bandwidth</li>
-                            <li>10 Domains</li>
-                            <li>15 Databases</li>
-                            <li>10 FTP Account</li>
-                            <li>50 Email Accounts</li>
-                        </ul>
-
-                        <footer className="plan-sign-up">
-                            <a className="button" href="">Sign Up</a>
-                        </footer>
-                    </div>
+                    <footer className="plan-sign-up">
+                        <a className="button" href="https://get.branch-bookkeeper.com">Install Now</a>
+                    </footer>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+);
+
+export default () => (
+    <Root id="pricing">
+        <div className="row section-head">
+            <h1>Pricing</h1>
+        </div>
+
+        <div className="row">
+            <div className="twelve columns">
+                <p>
+                    Branch Bookkeper is <strong>totally free for everyone</strong> while in beta.
+                </p>
+                <div className="plan-sign-up">
+                    <a className="button" href="https://get.branch-bookkeeper.com">Install Now</a>
+                </div>
+            </div>
+        </div>
+    </Root>
 );
