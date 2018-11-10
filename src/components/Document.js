969e731c8d1e009297e9dd248c729f92d6a99c4b
Gabriele Genta <gabriele.genta@gmail.com>
2018-11-10 17:43:21 +0100

    Add cookie bar

--- src/components/Document.js
+++ src/components/Document.js
@@ -32,6 +32,7 @@ const Document = ({
             <link rel="stylesheet" href="css/media-queries.css" />
             <link rel="stylesheet" href="css/animate.css" />
             <link rel="stylesheet" href="css/lightbox-fixes.css" />
+            <link rel="stylesheet" href="css/cookiebar-fixes.css" />
 
             <script src="js/modernizr.js" />
         </Head>
@@ -50,6 +51,8 @@ const Document = ({
             <script src="js/jquery.waypoints.min.js" />
             <script src="js/jquery.fittext.js" />
             <script src="js/main.js" />
+
+            <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=en&tracking=1&always=1&hideDetailsBtn=1" />
         </Body>
     </Html>
 );
